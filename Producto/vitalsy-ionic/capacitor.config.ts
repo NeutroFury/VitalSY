bf08d57dbd9d880084946f0616f6dbb7fc1d82aa
NeutroFury <jo.santanac@duocuc.cl>
2026-05-22 04:18:08 -0400

    Implementación de librerias para exportar a android

---
+++ Producto/vitalsy-ionic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vitalsy.app',
+  appName: 'VitalSY',
+  webDir: 'www'
+};
+
+export default config;
