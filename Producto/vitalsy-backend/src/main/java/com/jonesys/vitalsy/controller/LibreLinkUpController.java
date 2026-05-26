7563190ea0cce88e26c5bcec5073df55d2fb3b03
Gabeh <gabri.hernandeza@duocuc.cl>
2026-05-26 01:14:57 -0400

    Se actualiza rama personal y se genera actualización de la calculadora integrando la sustraccion de datos a partir del sensor, arrojando datos reales

--- Producto/vitalsy-backend/src/main/java/com/jonesys/vitalsy/controller/LibreLinkUpController.java
+++ Producto/vitalsy-backend/src/main/java/com/jonesys/vitalsy/controller/LibreLinkUpController.java
@@ -13,7 +13,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/librelinkup")
-@CrossOrigin(origins = "http://localhost:8100")
 public class LibreLinkUpController {
 
     private final LibreLinkUpConfigRepository configRepository;
